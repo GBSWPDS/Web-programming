bd8c21c7ced0dfdee32462b3bf61a330b19ed05e
806gw <shjea4364@naver.com>
2024-09-03 23:46:56 +0900

    feat: 전체 수정

--- web_programming_react/todolist/src/App.jsx
+++ web_programming_react/todolist/src/App.jsx
@@ -1,9 +1,45 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import TodoEditor from "./components/TodoEditor/TodoEditor";
 import TodoList from "./components/TodoList/TodoList";
 
+const mockTodo = [
+  {
+    id: 0,
+    content: "아무거나",
+    createDate: new Date().getTime(),
+    isDone: false,
+  },
+  {
+    id: 0,
+    content: "아무거나 1",
+    createDate: new Date().getTime(),
+    isDone: false,
+  },
+  {
+    id: 0,
+    content: "아무거나 2",
+    createDate: new Date().getTime(),
+    isDone: false,
+  },
+];
+
 function App() {
+  const [todo, setTodo] = useState(mockTodo);
+
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newItem = {
+      id: idRef.current,
+      content,
+      isDone: false,
+      createDate: new Date().getTime(),
+    };
+    setTodo([newItem, ...todo]);
+  };
+
   return (
     <div className="App">
       <Header />
